778d40dbb888e1a98874e999f0087c5a84e8ce41
Rishita Sanghavi <rishita.sanghavi07@gmail.com>
2026-03-16 14:07:26 +0530

    Update armstrongnumber.cpp

--- armstrongnumber.cpp
+++ armstrongnumber.cpp
@@ -1,9 +1,5 @@
 /*
-Armstrong Number :
-An Armstrong number (also known as a narcissistic number, pluperfect number, or pluperfect digital invariant) is a number that is
-equal to the sum of its own digits each raised to the power of the number of digits.
-For example, 153 is an Armstrong number because it has 3 digits,
-example : 1^3 + 5^3 + 3^3 = 153.
+Example : 1^3 + 5^3 + 3^3 = 153.
 */    
 #include <iostream>
 #include <cmath>    
