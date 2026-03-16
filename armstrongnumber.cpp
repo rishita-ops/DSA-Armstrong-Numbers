595e13a151d3c9319935bd529edd54164f988904
Rishita Sanghavi <rishita.sanghavi07@gmail.com>
2026-03-16 14:09:21 +0530

    Update armstrongnumber.cpp

--- armstrongnumber.cpp
+++ armstrongnumber.cpp
@@ -1,8 +1,7 @@
 /*
 Example : 1^3 + 5^3 + 3^3 = 153.
 */    
-#include <iostream>
-#include <cmath>    
+#include <bits/stdc++.h>   
 using namespace std;    
 int main() {    
     int num, originalNum, remainder, n = 0;    
